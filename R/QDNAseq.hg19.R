63cdaac5ffbf21b1476f3d831383e7f1aabfd329
Daoud Sie <d.sie@vumc.nl>
2015-08-14 10:49:00 +0200

    Created description for each dataset.

--- R/QDNAseq.hg19.R
+++ R/QDNAseq.hg19.R
@@ -5,64 +5,186 @@
 # 
 # \description{ The annotated data frame contains pre calculated data for
 # each bin describing:
-#   - chromosomal coordinate  
-#   - mappability
-#   - residual
-#   - GC content & N content
-#   - encode blacklist
-#   - DGV consensus blacklist
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
 # }
 #
 # @author "DS"
 #*/#########################################################################
 
+#########################################################################/**
+# @RdocData hg19.500kbp.SR50
+#
+# @title "hg19 500kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
 
-# library(QDNAseq)
-# library(Biobase)
-# library(BSgenome.Hsapiens.UCSC.hg19)
+#########################################################################/**
+# @RdocData hg19.100kbp.SR50
+#
+# @title "hg19 100kbp bin annotations"
 # 
-# binSize <- 1000
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg19.50kbp.SR50
+#
+# @title "hg19 50kbp bin annotations"
 # 
-# hg19.1000kbp.SR50 <- createBins(bsgenome=BSgenome.Hsapiens.UCSC.hg19, binSize=binSize)
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg19.30kbp.SR50
+#
+# @title "hg19 30kbp bin annotations"
 # 
-# bins$mappability <- calculateMappability(bins,
-#   bigWigFile='/path/to/wgEncodeCrgMapabilityAlign50mer.bigWig',
-#   bigWigAverageOverBed='/path/to/bigWigAverageOverBed')
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg19.15kbp.SR50
+#
+# @title "hg19 15kbp bin annotations"
 # 
-# bins$blacklist <- calculateBlacklist(bins,
-#   bedFiles=c('/path/to/wgEncodeDacMapabilityConsensusExcludable.bed',
-#     '/path/to/wgEncodeDukeMapabilityRegionsExcludable.bed'))
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg19.10kbp.SR50
+#
+# @title "hg19 10kbp bin annotations"
 # 
-# # 1kG data
-# ctrl <- binReadCounts(bins, path='/path/to/control-set/bam/files')
-# ctrl <- applyFilters(ctrl, residual=FALSE, blacklist=FALSE, mappability=FALSE, bases=FALSE)
-# bins$residual <- iterateResiduals(ctrl)
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg19.5kbp.SR50
+#
+# @title "hg19 5kbp bin annotations"
 # 
-# bins$use <- bins$chromosome %in% as.character(1:22) & bins$bases > 0
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg19.1kbp.SR50
+#
+# @title "hg19 1kbp bin annotations"
 # 
-# bins <- AnnotatedDataFrame(bins, varMetadata=data.frame(labelDescription=c(
-#   'Chromosome name',
-#   'Base pair start position',
-#   'Base pair end position',
-#   'Percentage of non-N nucleotides (of full bin size)',
-#   'Percentage of C and G nucleotides (of non-N nucleotides)',
-#   'Average mappability of 50mers with a maximum of 2 mismatches',
-#   'Percent overlap with ENCODE blacklisted regions',
-#   'Median loess residual from 1000 Genomes (50mers)',
-#   'Whether the bin should be used in subsequent analysis steps'),
-#   row.names=colnames(bins)))
-# 
-# attr(bins, "QDNAseq") <- list(
-#   author="Ilari Scheinin",
-#   date=Sys.time(),
-#   organism="Hsapiens",
-#   build="hg19",
-#   version=packageVersion("QDNAseq"),
-#   md5=digest::digest(bins@data),
-#   sessionInfo=sessionInfo())
-# 
-# # DGV?
-# 
-# save(hg19.1000kbp.SR50, file="hg19.1000kbp.SR50.rda", compress="xz")
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
 
 # EOF
